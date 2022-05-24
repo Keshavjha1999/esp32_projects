71e7045e47807bac537f06931236fe6515d99c4c
Keshav <keshavkjha1999@gmail.com>
2022-05-24 10:46:37 +0530

    Initial commit

--- humidity-temperature-sensor/main/tasks_common.h
+++ humidity-temperature-sensor/main/tasks_common.h
@@ -18,9 +18,9 @@
 
 
 // DHT22 Sensor task
-#define DHT11_TASK_STACK_SIZE                       4096
-#define DHT11_TASK_PRIORITY                         5
-#define DHT11_TASK_CORE_ID                          1
+#define DHT22_TASK_STACK_SIZE                       4096
+#define DHT22_TASK_PRIORITY                         5
+#define DHT22_TASK_CORE_ID                          1
 
 
 #endif /* MAIN_TASKS_COMMON_H_ */
